ff57a243147c857026b539b6cc63f7011486e17e
Ruth <aylaaeternum@gmail.com>
2022-11-03 09:44:43 +0100

    module 111 2

---
+++ js/js.js
@@ -0,0 +1,20 @@
+
+
+
+//1 create a var that stores the name that the user enters via prompt.
+
+var name = prompt("what is your name");
+
+//2 Capitalise the first letter of their name
+
+// A: Isolate the first char
+
+// B: Turn the first char to uppercase
+
+// C: Isolate the rest of the name
+
+// D: Concactenate the first char with the rest of the char
+
+//3 We use the capitalise version of their name to greet them using an alert
+
+//Hello, Angela. 
