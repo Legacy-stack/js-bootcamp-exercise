f9afae5a6d91100412b3c071545c51fbb8b7b02d
Ruth <aylaaeternum@gmail.com>
2023-07-10 06:50:08 +0200

    bootcamp

--- TinDog-Start-master/app.js
+++ TinDog-Start-master/app.js
@@ -1,11 +1,1 @@
-
-const MY_TITLE = document.getElementsByTagName("h1")[0];
-const MY_BUTTON = document.getElementById('mybutton');
-const MY_INPUT = document.getElementById('myInput');
-
-const MY_P = document.getElementsByTagName('p');
-
-MY_BUTTON.addEventListener('click', () => {
-    MY_TITLE.style.color = MY_INPUT.value;
-});
-
+const lista = document.getElementsByTagName('li');
