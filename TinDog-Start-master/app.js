2d40b419974c8bfe9b258baf9e396c80d6804c3d
Ruth <aylaaeternum@gmail.com>
2023-07-12 07:00:42 +0200

    bootcamp

--- TinDog-Start-master/app.js
+++ TinDog-Start-master/app.js
@@ -1,1 +1,13 @@
-const lista = document.getElementsByTagName('li');
+/*const lista = document.getElementsByTagName('li');
+
+for (let i = 0; i <lista.length; i += 1 ){
+    lista[i].style.color = 'green';
+}*/
+
+const input = document.querySelector('input');
+const pixel = document.querySelector('pixel');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    p.textContent = input.value + ':';
+})
